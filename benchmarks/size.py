896fe0d3dac98ff2eadce7a6b22ef67ae9e5f8f3
William Silversmith <william.silversmith@gmail.com>
2022-12-06 16:28:31 -0500

    feat: add size comparison benchmark

---
+++ benchmarks/size.py
@@ -0,0 +1,56 @@
+import crackle
+import compresso
+
+import numpy as np
+import zlib
+import fastremap
+from cloudvolume import view
+
+def sample(labels, shape):
+  x,y,z = tuple(np.random.randint(128,384, size=(3,)))
+  return labels[x:x+256,y:y+256,z:z+32]
+
+def run_sample(labels, shape, N):
+  for i in range(10):
+    cutout = sample(labels, shape)
+    
+    ckl_binary = crackle.compress(cutout)
+    cpso_binary = compresso.compress(cutout)
+    raw_binary = cutout.tobytes('F')
+
+    cckl_binary = zlib.compress(ckl_binary)
+    ccpso_binary = zlib.compress(cpso_binary)
+    craw_binary = zlib.compress(raw_binary)
+
+    print(f"""
+      ckl:     {len(ckl_binary)}       
+      cpso:    {len(cpso_binary)}   ({len(ckl_binary)/len(cpso_binary)*100:.2f}%)
+      raw:     {len(raw_binary)}   ({len(ckl_binary)/len(raw_binary)*100:.2f}%)
+      ckl.gz:  {len(cckl_binary)}      
+      cpso.gz  {len(ccpso_binary)}  ({len(cckl_binary)/len(ccpso_binary)*100:.2f}%)
+      raw:.gz  {len(craw_binary)}  ({len(cckl_binary)/len(craw_binary)*100:.2f}%)
+    """)
+
+N = 10
+shape = (128,128,64)
+
+print("PINKY40 CUTOUTS")
+labels = compresso.load("benchmarks/connectomics.npy.cpso.gz")
+run_sample(labels, shape, N)
+
+print("WATERSHED CUTOUTS")
+labels = compresso.load("benchmarks/ws.npy.cpso.gz")
+run_sample(labels, shape, N)
+
+print("RANDOM NOISE")
+labels = np.random.randint(0,2000, size=(512,512,512), dtype=np.uint32)
+run_sample(labels, shape, N)
+
+print("EMPTY")
+labels = np.zeros((512,512,512), dtype=np.uint32)
+run_sample(labels, shape, 1)
+
+print("SOLID ONES")
+labels = np.ones((512,512,512), dtype=np.uint32)
+run_sample(labels, shape, 1)
+
