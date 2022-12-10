240641a0274398aebddbe3a87967319d10c14449
William Silversmith <william.silversmith@gmail.com>
2022-12-09 20:03:42 -0500

    fix: correct casting

--- src/fastcrackle.cpp
+++ src/fastcrackle.cpp
@@ -8,12 +8,15 @@
 namespace py = pybind11;
 
 template <typename LABEL>
-py::array decompress_helper(const py::bytes &buffer) {
+py::array decompress_helper(
+	const crackle::CrackleHeader& head, 
+	const py::bytes &buffer
+) {
 	py::array arr = py::array_t<LABEL>(head.voxels());
 	crackle::decompress<LABEL>(
 		buffer, 
 		/*fortran_order=*/true,
-		const_cast<LABEL*>(arr.data())
+		reinterpret_cast<LABEL*>(const_cast<void*>(arr.data()))
 	);
 	return arr;
 }
@@ -22,16 +25,16 @@ py::array decompress(const py::bytes &buffer) {
 	crackle::CrackleHeader head(buffer);
 
 	if (head.data_width == 1) {
-		return decompress_helper<uint8_t>(buffer);
+		return decompress_helper<uint8_t>(head, buffer);
 	}
 	else if (head.data_width == 2) {
-		return decompress_helper<uint16_t>(buffer);
+		return decompress_helper<uint16_t>(head, buffer);
 	}
 	else if (head.data_width == 4) {
-		return decompress_helper<uint32_t>(buffer);	
+		return decompress_helper<uint32_t>(head, buffer);	
 	}
 	else {
-		return decompress_helper<uint64_t>(buffer);
+		return decompress_helper<uint64_t>(head, buffer);
 	}
 }
 
