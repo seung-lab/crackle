48e2824a7a88c65e557cf6e5ed9104c726920131
William Silversmith <william.silversmith@gmail.com>
2022-12-10 10:45:28 -0500

    perf: faster pin computation

--- crackle/pins.py
+++ crackle/pins.py
@@ -55,6 +55,7 @@ def find_optimal_pins(pinset):
     universe -= cur
     for j, otherset in isets:
       otherset -= cur
+    isets = [ x for x in isets if len(x[1]) > 0 ]
     final_pins.append(pinset[i][:2])
 
   return final_pins
