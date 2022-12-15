3f3fa32bf2a57359fc9ae081e7b1d83a6fd559f7
William Silversmith <william.silversmith@gmail.com>
2022-12-15 13:41:57 -0500

    wip(pins.hpp): adding column extraction

--- src/pins.hpp
+++ src/pins.hpp
@@ -36,19 +36,69 @@ struct Pin {
 };
 
 struct CandidatePin {
-	uint64_t startpt;
-	uint64_t endpt;
+	uint32_t x;
+	uint32_t y;
+	uint32_t z_s;
+	uint32_t z_e;
 	std::unordered_set<uint32_t> ccids;
 
 	CandidatePin() {
-		startpt = 0;
-		endpt = 0;
+		x = 0;
+		y = 0;
+		z_s = 0;
+		z_e = 0;
 	}
 
-	CandidatePin(uint64_t _startpt, uint64_t _endpt, std::unordered_set<uint32_t> _ccids) 
-		: startpt(_startpt), endpt(_endpt), ccids(_ccids)
+	CandidatePin(
+		const uint32_t _x,
+		const uint32_t _y,
+		const uint32_t _z_s,
+		const uint32_t _z_e,
+		std::unordered_set<uint32_t> _ccids) 
+		: x(_x), y(_y), z_s(_z_s), 
+		  z_e(_z_e), ccids(_ccids)
 	{}
+
+	uint64_t start_idx(uint64_t sx, uint64_t sy) {
+		return static_cast<uint64_t>(x) + sx * (static_cast<uint64_t>(y) + sy * static_cast<uint64_t>(z_s));
+	}
+	uint64_t depth() {
+		return static_cast<uint64_t>(z_e - z_s);
+	}
 };
+
+
+template <typename LABEL>
+void add_pin(
+	std::unordered_map<LABEL, std::vector<CandidatePin>> &pinsets,
+	LABEL label,
+	const uint64_t z_start,
+	const uint64_t x, const uint64_t y, const uint64_t z,
+	const uint64_t sx, const uint64_t sy, const uint64_t sz,
+	const std::unordered_set<uint32_t> &cc_set
+) {
+	if (pinsets[label].size() == 0) {
+		pinsets[label].emplace_back(x, y, z_start, z, cc_set);
+		return;
+	}
+
+	CandidatePin last_pin = pinsets[label].back();
+
+	if (last_pin.x == x - 1 && last_pin.y == y) {
+		if (last_pin.z_s <= z_start && last_pin.z_e >= z) {
+			return;
+		}
+		else if (last_pin.z_s >= z_start && last_pin.z_e <= z) {
+			pinsets[label].back() = CandidatePin(x, y, z_start, cc_set);
+		}
+		else {
+			pinsets[label].emplace_back(x, y, z_start, z, cc_set);
+		}
+	}
+	else {
+		pinsets[label].emplace_back(x, y, z_start, z, cc_set);
+	}
+}
 
 template <typename LABEL>
 std::vector<CandidatePin> extract_columns(
@@ -73,16 +123,19 @@ std::vector<CandidatePin> extract_columns(
 
 			std::unordered_set<uint32_t> label_set;
 			label_set.insert(cc_labels[loc]);
+			uint64_t z_start = 0;
 			for (uint64_t z = 0; z < sz; z++) {
 				uint64_t zoff = sx * sy * z;
 				LABEL cur = labels[loc + zoff];
 				if (label != cur) {
-					pinsets[label].emplace_back(loc, loc+zoff, label_set);
+					add_pin(pinsets, label, z_start, x, y, z-1, label_set);
 					label = cur;
+					z_start = z;
 					label_set.clear();
 				}
 				label_set.insert(cc_labels[loc + zoff]);
 			}
+			add_pin(pinsets, label, z_start, x, y, z, label_set);
 		}
 	}
 
