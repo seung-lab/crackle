df1f31033d388289ead7539d8ab72ec29b613618
William Silversmith <william.silversmith@gmail.com>
2022-12-29 13:03:09 -0500

    benchmarks: add encoding speed estimates

--- benchmarks/size.py
+++ benchmarks/size.py
@@ -4,6 +4,8 @@ import compresso
 import numpy as np
 import zlib
 
+import time
+
 def sample(labels, shape):
   x,y,z = tuple(np.random.randint(0,max(labels.shape) - max(shape), size=(3,)))
   return labels[x:x+shape[0],y:y+shape[1],z:z+shape[2]]
@@ -12,19 +14,32 @@ def run_sample(labels, shape, N):
   for i in range(N):
     cutout = sample(labels, shape)
     
-    ckl_binary = crackle.compress(cutout)
+    s = time.time()
+    ckl_binary = crackle.compress(cutout, force_flat=True)
+    compress_time = time.time() - s
+
+    s = time.time()
     cpso_binary = compresso.compress(cutout)
+    cpso_time = time.time() - s
+
     raw_binary = cutout.tobytes('F')
 
+    s = time.time()
     cckl_binary = zlib.compress(ckl_binary)
+    compress_time_gz = compress_time + (time.time() - s)
+
     ccpso_binary = zlib.compress(cpso_binary)
     craw_binary = zlib.compress(raw_binary)
 
+    mvxs = cutout.size / compress_time / 1e6
+    mvxs_gz = cutout.size / compress_time_gz / 1e6
+    mvxs_cpso = cutout.size / cpso_time / 1e6
+
     print(f"""
-      ckl:     {len(ckl_binary)}       
-      cpso:    {len(cpso_binary)}   ({len(ckl_binary)/len(cpso_binary)*100:.2f}%)
+      ckl:     {len(ckl_binary)}    ({mvxs:.2f} MVx/sec)
+      cpso:    {len(cpso_binary)}   ({len(ckl_binary)/len(cpso_binary)*100:.2f}%) ({mvxs_cpso:.2f} MVx/sec)
       raw:     {len(raw_binary)}   ({len(ckl_binary)/len(raw_binary)*100:.2f}%)
-      ckl.gz:  {len(cckl_binary)}      
+      ckl.gz:  {len(cckl_binary)}   ({mvxs_gz:.2f} MVx/sec)
       cpso.gz  {len(ccpso_binary)}  ({len(cckl_binary)/len(ccpso_binary)*100:.2f}%)
       raw:.gz  {len(craw_binary)}  ({len(cckl_binary)/len(craw_binary)*100:.2f}%)
     """, flush=True)
