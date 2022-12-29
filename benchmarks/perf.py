67aab9eb0250df9ba3cad08d8ac15d1f2133d834
William Silversmith <william.silversmith@gmail.com>
2022-12-29 16:51:23 -0500

    feat: add performance benchmark

---
+++ benchmarks/perf.py
@@ -0,0 +1,66 @@
+import crackle
+import compresso
+
+import numpy as np
+import deflate
+
+import time
+
+def sample(labels, shape):
+  x,y,z = tuple(np.random.randint(0,max(labels.shape) - max(shape), size=(3,)))
+  return labels[x:x+shape[0],y:y+shape[1],z:z+shape[2]]
+
+def run_sample(labels, shape, N):
+  for i in range(N):
+    cutout = sample(labels, shape)
+    
+    s = time.time()
+    ckl_binary = crackle.compress(cutout, force_flat=True)
+    compress_time = time.time() - s
+
+    s = time.time()
+    gzip_bin = deflate.gzip_compress(ckl_binary, 6)
+    gzip_time = time.time() - s
+
+    s = time.time()
+    deflate.gzip_decompress(gzip_bin)
+    gunzip_time = time.time() - s
+
+    s = time.time()
+    crackle.decompress(ckl_binary)
+    decompress_time = time.time() - s    
+
+    mvxs = lambda t: cutout.size / t / 1e6
+
+    print(f"""
+      compress     :  {mvxs(compress_time):.2f} MVx/sec
+      compress+gz  :  {mvxs(compress_time+gzip_time):.2f} MVx/sec
+      decompress   :  {mvxs(decompress_time):.2f} MVx/sec
+      decompress+gz:  {mvxs(decompress_time+gunzip_time):.2f} MVx/sec
+    """, flush=True)
+
+N = 3
+shape = (256,256,64)
+
+print("shape:", shape)
+
+print("PINKY40 CUTOUTS (connectomics.npy)")
+labels = compresso.load("benchmarks/connectomics.npy.cpso.gz")
+run_sample(labels, shape, N)
+
+print("WATERSHED CUTOUTS (ws.npy)")
+labels = compresso.load("benchmarks/ws.npy.cpso.gz")
+run_sample(labels, shape, N)
+
+print("RANDOM NOISE [0,2000) uint32")
+labels = np.random.randint(0,2000, size=(512,512,512), dtype=np.uint32)
+run_sample(labels, shape, N)
+
+print("EMPTY")
+labels = np.zeros((512,512,512), dtype=np.uint32)
+run_sample(labels, shape, 1)
+
+print("SOLID ONES")
+labels = np.ones((512,512,512), dtype=np.uint32)
+run_sample(labels, shape, 1)
+
