eb6ff669ee7ecbb2d149b8011c1fa23be584c673
William Silversmith <william.silversmith@gmail.com>
2022-12-30 22:16:34 -0500

    test: pare down tests to C++ code

--- automated_test.py
+++ automated_test.py
@@ -1,55 +1,9 @@
 import crackle
-import cracklepy.crackcode
 import numpy as np
 
 import compresso
 
 import pytest
-
-def test_create_graph():
-  labels = np.zeros((3,3), dtype=np.uint32)
-  labels[1,1] = 1
-
-  G = cracklepy.crackcode.create_graph(labels)
-
-  assert set(G.nodes) == set([5,9,6,10])
-  assert set(G.edges) == set([(5,9), (5,6), (6,10), (9,10)])
-
-def test_create_crack_codes():
-  labels = np.zeros((3,3), dtype=np.uint32)
-  labels[1,1] = 1
-
-  codes = cracklepy.crackcode.create_crack_codes(labels, permissible=False)
-  ans = [[9, 1, 0, 2, 3, 3, 0]]
-  assert codes == ans
-
-@pytest.mark.parametrize("size", [16, 64, 128, 1024])
-def test_packed_encoding(size):
-  chains = [[9, 0, 3, 1, 0, 2, 3, 3, 0, 1, 2]]
-  ans = { 9: ['b', 1, 0, 2, 3, 't', 't'] }
-
-  sx = sy = size
-
-  packed_code = cracklepy.crackcode.pack_codes(chains, sx, sy)
-  recovered = cracklepy.crackcode.unpack_binary(packed_code, sx, sy)
-
-  assert ans == recovered
-
-def test_remove_initial_branch():
-  code = []
-  assert cracklepy.crackcode.remove_initial_branch(code, 5, 5) == []
-
-  code = [10, 'd','u','t']
-  assert cracklepy.crackcode.remove_initial_branch(code, 5, 5) == code
-
-  code = [10, 'b', 'd', 'r', 't']
-  revised = cracklepy.crackcode.remove_initial_branch(code, 5, 5)
-  assert revised == [17, 's', 's', 'l', 'u']
-
-  code = [10, 'b', 'd', 'b', 'r', 't']
-  revised = cracklepy.crackcode.remove_initial_branch(code, 5, 5)
-  assert revised == [10, 'b', 'd', 'b', 'r', 't']
-
 
 def test_compress_decompress_empty():
   labels = np.zeros((100,100,32), dtype=np.uint32)
