29b3d2c782d95db8a69f765ccfd583641f8e4b34
William Silversmith <william.silversmith@gmail.com>
2023-01-04 00:35:49 -0500

    build: fix extra compile options for mvcc

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 import setuptools
 from pybind11.setup_helpers import Pybind11Extension, build_ext
+import sys
+
+extra_compile_args = []
+if sys.platform == 'win32':
+  extra_compile_args += [
+    '/std:c++17', '/O2'
+  ]
+else:
+  extra_compile_args += [
+    '-std=c++17', '-O3'
+  ]
 
 setuptools.setup(
   setup_requires=['pbr','pybind11','numpy'],
@@ -8,7 +19,7 @@ setuptools.setup(
     Pybind11Extension(
         "fastcrackle",
         ["src/fastcrackle.cpp"],
-        extra_compile_args=["-std=c++17"],
+        extra_compile_args=extra_compile_args,
         language="c++",
     ),
   ],
