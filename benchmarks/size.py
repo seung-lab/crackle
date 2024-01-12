64af0a1fb21dbf49dbe7cef68ef9d1735a18b742
William Silversmith <william.silversmith@gmail.com>
2024-01-11 20:01:44 -0500

    docs: update benchmark
    More useful comparison ratios and adds brotli

--- benchmarks/size.py
+++ benchmarks/size.py
@@ -3,6 +3,7 @@ import compresso
 
 import numpy as np
 import zlib
+import brotli
 
 import time
 
@@ -16,47 +17,39 @@ def run_sample(labels, shape, N):
     run(cutout)
 
 def run(labels):
-    s = time.time()
-    ckl_binary = crackle.compress(labels, allow_pins=False)
-    compress_time = time.time() - s
+  ckl_binary = crackle.compress(labels, allow_pins=False)    
+  mkv_binary = crackle.compress(labels, allow_pins=False, markov_model_order=5)
+  pin_binary = crackle.compress(labels, allow_pins=True)
+  cpso_binary = compresso.compress(labels)
+  raw_binary = labels.tobytes('F')
 
-    s = time.time()
-    mkv_binary = crackle.compress(labels, allow_pins=False, markov_model_order=5)
-    mkv_compress_time = time.time() - s
+  for method in ['','gz','br']:
 
-    s = time.time()
-    pin_binary = crackle.compress(labels, allow_pins=True)
-    pin_compress_time = time.time() - s
 
-    s = time.time()
-    cpso_binary = compresso.compress(labels)
-    cpso_time = time.time() - s
+    if method == '':
+      fn = lambda x: x
+      ext = ''
+    elif method == 'gz':
+      fn = zlib.compress
+      ext = '.gz'
+    elif method == 'br':
+      fn = brotli.compress
+      ext = '.br'
 
-    raw_binary = labels.tobytes('F')
-
-    s = time.time()
-    cckl_binary = zlib.compress(ckl_binary)
-    compress_time_gz = compress_time + (time.time() - s)
-
-    ccpso_binary = zlib.compress(cpso_binary)
-    craw_binary = zlib.compress(raw_binary)
-    cpin_binary = zlib.compress(pin_binary)
-    cmkv_binary = zlib.compress(mkv_binary)
+    cckl_binary = fn(ckl_binary)
+    ccpso_binary = fn(cpso_binary)
+    craw_binary = fn(raw_binary)
+    cpin_binary = fn(pin_binary)
+    cmkv_binary = fn(mkv_binary)
 
     print(f"""
-      ckl:         {len(ckl_binary): 9}
-      ckl(pin):    {len(pin_binary): 9}  ({len(pin_binary)/len(ckl_binary)*100:.2f}%) (vs flat)
-      ckl(mkv):    {len(mkv_binary): 9}  ({len(mkv_binary)/len(ckl_binary)*100:.2f}%) (vs literals)
-      cpso:        {len(cpso_binary): 9}  ({len(ckl_binary)/len(cpso_binary)*100:.2f}%) 
-      raw:         {len(raw_binary): 9}  ({len(ckl_binary)/len(raw_binary)*100:.2f}%)
-      ckl.gz:      {len(cckl_binary): 9}   
-      ckl.gz(pin): {len(cpin_binary): 9}   ({len(cpin_binary)/len(cckl_binary)*100:.2f}%) (vs flat)
-      ckl.gz(mkv): {len(cmkv_binary): 9}   ({len(cmkv_binary)/len(cckl_binary)*100:.2f}%) (vs literals)
-      cpso.gz:     {len(ccpso_binary): 9}  ({len(cckl_binary)/len(ccpso_binary)*100:.2f}%)
-      raw.gz:      {len(craw_binary): 9}  ({len(cckl_binary)/len(craw_binary)*100:.2f}%)
-    """, flush=True)
+      ckl{ext}:      {len(cckl_binary): 9}   ({len(cckl_binary)/len(craw_binary)*100:.2f}%)
+      ckl{ext}(pin): {len(cpin_binary): 9}   ({len(cpin_binary)/len(craw_binary)*100:.2f}%) 
+      ckl{ext}(mkv): {len(cmkv_binary): 9}   ({len(cmkv_binary)/len(craw_binary)*100:.2f}%)
+      cpso{ext}:     {len(ccpso_binary): 9}  ({len(ccpso_binary)/len(craw_binary)*100:.2f}%)
+      raw{ext}:      {len(craw_binary): 9}  ({len(craw_binary)/len(craw_binary)*100:.2f}%)""", flush=True)
 
-N = 3
+N = 1
 shape = (256,256,64)
 
 print("shape:", shape)
@@ -74,14 +67,6 @@ print("WATERSHED CUTOUTS (ws.npy)")
 labels = compresso.load("benchmarks/ws.npy.cpso.gz")
 run_sample(labels, shape, N)
 
-print("RANDOM NOISE [0,2000) uint32")
-labels = np.random.randint(0,2000, size=(512,512,512), dtype=np.uint32)
-run_sample(labels, shape, N)
-
-print("BINARY NOISE [0,1] uint8 (pathological case)")
-labels = np.random.randint(0,2, size=(512,512,512), dtype=np.uint8)
-run_sample(labels, shape, N)
-
 print("EMPTY")
 labels = np.zeros((512,512,512), dtype=np.uint32)
 run_sample(labels, shape, 1)
@@ -90,3 +75,10 @@ print("SOLID ONES")
 labels = np.ones((512,512,512), dtype=np.uint32)
 run_sample(labels, shape, 1)
 
+print("RANDOM NOISE [0,2000) uint32")
+labels = np.random.randint(0,2000, size=(512,512,128), dtype=np.uint32)
+run_sample(labels, shape, N)
+
+print("BINARY NOISE [0,1] uint8 (pathological case)")
+labels = np.random.randint(0,2, size=(512,512,128), dtype=np.uint8)
+run_sample(labels, shape, N)
