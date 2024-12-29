3e6cdfea1f21d21643e7ae97accb599be040f768
William Silversmith <william.silversmith@gmail.com>
2024-12-29 01:33:59 -0500

    fix: special handling for single voxel contours

--- src/dual_graph.hpp
+++ src/dual_graph.hpp
@@ -372,7 +372,15 @@ merge_holes(
 	// printf("sz: %d\n", candidate_contours.size());
 
 	for (uint64_t i = 0; i < candidate_contours.size(); i++) {
+		if (candidate_contours[i].size() == 1) {
+			continue;
+		}
+
 		for (uint64_t j = i + 1; j < candidate_contours.size(); j++) {
+			if (candidate_contours[j].size() == 1) {
+				continue;
+			}
+
 			auto& bbx1 = bboxes[i];
 			auto& bbx2 = bboxes[j];
 
