fix: corrected zstack, also fixed bug in zindex calculation

crackle/codec.py
@@ -117,14 +117,16 @@ def component_lengths(binary:bytes):
   return { k:len(v) for k,v in components(binary).items() }
 
 def crack_codes(binary:bytes) -> np.ndarray:
-  header = CrackleHeader.frombytes(binary)
-  comps = components(binary)
+  head = CrackleHeader.frombytes(binary)
+  
+  offset = head.header_bytes
+  z_index_binary = binary[offset:offset + head.grid_index_bytes]
 
-  if header.format_version == 0:
-    z_index = np.frombuffer(comps["z_index"], dtype=np.uint32)
+  if head.format_version == 0:
+    z_index = np.frombuffer(z_index_binary, dtype=np.uint32)
   else:
-    z_index = np.frombuffer(comps["z_index"][:-4], dtype=np.uint32)
-    stored_crc32c = int.from_bytes(comps["z_index"][-4:], 'little')
+    z_index = np.frombuffer(z_index_binary[:-4], dtype=np.uint32)
+    stored_crc32c = int.from_bytes(z_index_binary[-4:], 'little')
     computed_crc32c = crc32c(z_index)
     if stored_crc32c != computed_crc32c:
       raise FormatError(f"Grid index CRC32C did not match stored version. Stored: {stored_crc32c} Computed: {computed_crc32c}")
@@ -132,14 +134,14 @@ def crack_codes(binary:bytes) -> np.ndarray:
   z_index = np.concatenate([ [0], z_index ])
   z_index = np.cumsum(z_index)
   z_index += (
-    len(header.tobytes()) 
-    + header.num_label_bytes 
-    + header.sz * header.z_index_width()
+    head.header_bytes
+    + head.num_label_bytes 
+    + head.grid_index_bytes
   )
   z_index = z_index.astype(np.uint64)
   
   codes = []
-  for i in range(header.sz):
+  for i in range(head.sz):
     codes.append(
       binary[z_index[i]:z_index[i+1]]
     )

crackle/operations.py
@@ -397,7 +397,7 @@ def zstack(images:Sequence[Union[np.ndarray, bytes]]) -> bytes:
     raise ValueError(f"Unsupported label format: {first_head.label_format}")
 
   crack_codes_lst = []
-  zindex = np.zeros((first_head.sz,), dtype=np.uint32)
+  zindex = np.zeros((sz,), dtype=np.uint32)
   z = 0
   for binary in binaries:
     for cc in crack_codes(binary):