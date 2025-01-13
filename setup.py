be3539841382fe779d10471361a2f018340e5d0f
William Silversmith <william.silversmith@gmail.com>
2025-01-12 23:28:57 -0500

    refactor: try using only fastcrc (easier build)

--- setup.py
+++ setup.py
@@ -1,69 +1,10 @@
-import multiprocessing as mp
 import os
-import platform
 import setuptools
-import shutil
-import subprocess
 import sys
 
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-CRC32C_DIR = os.path.join("third_party", "crc32c")
-CRC32C_HIGH_SPEED_DIR = os.path.join("third_party", "fastcrc")
-
-CRC32C_INCLUDE_DIR = os.path.join(CRC32C_DIR, "include")
-CRC32C_BUILD_DIR = os.path.join(CRC32C_DIR, "build")
-WINDOWS_CRC32C_BUILD_DIR = os.path.join(CRC32C_BUILD_DIR, "Release")
-
-def build_crc32c():
-  library_names = [
-    "libcrc32c.a",
-    "crc32c.lib"
-  ]
-
-  exists = [ 
-    os.path.exists(os.path.join(CRC32C_BUILD_DIR, name)) 
-    for name in library_names
-  ]
-
-  if any(exists):
-    print("crackle: libcrc32c already built.")
-    return
-
-  if os.path.exists(CRC32C_BUILD_DIR):
-    shutil.rmtree(CRC32C_BUILD_DIR)
-  else:
-    os.makedirs(CRC32C_BUILD_DIR)
-
-  res = subprocess.check_call([
-    "cmake", 
-    f"-DCRC32C_BUILD_TESTS=0",
-    f"-DCRC32C_BUILD_BENCHMARKS=0",
-    f"-DBUILD_SHARED_LIBS=OFF",
-    f"-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
-    f"-DCMAKE_OSX_DEPLOYMENT_TARGET=11.0",
-    f"-S {CRC32C_DIR}", 
-    f"-B {CRC32C_BUILD_DIR}",
-    "-DCMAKE_BUILD_TYPE=Release",
-  ])
-
-  if res != 0:
-    print("crackle: unable to cmake libcrc32c.")
-    return
-
-  res = subprocess.check_call([
-    "cmake", 
-    f"--build",
-    f"{CRC32C_BUILD_DIR}",
-    "--config", 
-    "Release",
-  ])
-
-  if res != 0:
-    print("crackle: unable to build libcrc32c.")
-    return
-
-build_crc32c()
+CRC32C_DIR = os.path.join("third_party", "fastcrc")
 
 extra_compile_args = []
 if sys.platform == 'win32':
@@ -74,12 +15,6 @@ else:
   extra_compile_args += [
     '-std=c++2a', '-O3'
   ]
-
-operating_system = platform.system().lower()
-is_windows = sys.platform == 'win32' or operating_system == "windows"
-library_dir = CRC32C_BUILD_DIR 
-if is_windows:
-  library_dir = WINDOWS_CRC32C_BUILD_DIR
 
 setuptools.setup(
   setup_requires=['pbr','pybind11','numpy'],
@@ -93,9 +28,7 @@ setuptools.setup(
     Pybind11Extension(
         "fastcrackle",
         ["src/fastcrackle.cpp"],
-        include_dirs=[CRC32C_INCLUDE_DIR, CRC32C_HIGH_SPEED_DIR],
-        libraries=["crc32c"],
-        library_dirs=[library_dir],
+        include_dirs=[CRC32C_DIR],
         extra_compile_args=extra_compile_args,
         language="c++",
     ),
