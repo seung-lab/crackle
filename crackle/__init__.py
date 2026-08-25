3037e7215b8b947e55c0edf8111aec7d3cd6e1cf
William Silversmith <william.silversmith@gmail.com>
2026-08-24 23:49:27 -0400

    feat: export component_map

--- crackle/__init__.py
+++ crackle/__init__.py
@@ -59,6 +59,7 @@ from .operations import (
 	contacts,
 	array_equal,
 	mode_pooling_2x2x1,
+	component_map,
 )
 from .headers import FormatError, CrackleHeader
 from .util import save, load, aload, bload, save_numpy
